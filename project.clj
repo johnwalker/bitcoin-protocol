d918c4c7d617dd6b170f29eedf73551b8e24a04e
John Walker <john.lou.walker@gmail.com>
2014-05-25 10:02:48 -0700

    fixup! 0.1.3 -> 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bitcoin-protocol "0.3.0-SNAPSHOT"
+(defproject bitcoin-protocol "0.3.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
