25957c2fc913a66036a1f28179e17a89a670f428
John Walker <john.lou.walker@gmail.com>
2014-05-25 08:54:31 -0700

    Add expected-bytes utility function

--- test/bitcoin_protocol/messages_test.clj
+++ test/bitcoin_protocol/messages_test.clj
@@ -30,4 +30,7 @@
          (fact "Char array of \"/Satoshi:0.7.2/\" is correct"
                (str-bytes satoshi-str) => "2F 53 61 74 6F 73 68 69 3A 30 2E 37 2E 32 2F")))
 
+(defn expected-bytes [length]
+  (+ (dec length)
+     (* 2 length)))
 
