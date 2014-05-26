2c7ae2c78758cd5e6ee9f35a9375e94f60624f66
John Walker <john.lou.walker@gmail.com>
2014-05-25 19:42:39 -0700

    0.5.0 -> 0.6.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bitcoin-protocol "0.5.0"
+(defproject bitcoin-protocol "0.6.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
