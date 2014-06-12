9243d3918164093e7024981a9ada61c0fb6a5f77
John Walker <john.lou.walker@gmail.com>
2014-06-11 23:03:14 -0700

    Implement @ztellman's performance suggestions

--- src/bitcoin_protocol/messages.clj
+++ src/bitcoin_protocol/messages.clj
@@ -3,22 +3,24 @@
             [gloss.core :refer :all]
             [gloss.io :refer :all]))
 
-(defcodec varint (header :ubyte
-                         (fn [h]
-                           (compile-frame
-                            (case h
-                              0xfd :uint16-le
-                              0xfe :uint32-le
-                              0xff :uint64-le
-                              (compile-frame nil-frame
-                                             identity
-                                             (fn [_] h)))))
-                         (fn [b]
-                           (condp >= b
-                             0xfc b
-                             0xffff 0xfd
-                             0xffffffff 0xfe
-                             0xff))))
+
+
+(let [u16-le (compile-frame :uint16-le)
+      u32-le (compile-frame :uint32-le)
+      u64-le (compile-frame :uint64-le)]
+  (defcodec varint (header :ubyte
+                           (fn [h]
+                             (case h
+                               0xfd u16-le
+                               0xfe u32-le
+                               0xff u64-le
+                               nil-frame))
+                           (fn [b]
+                             (condp >= b
+                               0xfc b
+                               0xffff 0xfd
+                               0xffffffff 0xfe
+                               0xff)))))
 
 (defcodec varstr (finite-frame
                   varint
