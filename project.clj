c517f73ac278b0f4f73bf016c1f8cec1c4b3eb2e
John Walker <john.lou.walker@gmail.com>
2014-07-04 00:50:23 -0700

    minor inc version + mavenny rename

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject bitcoin-protocol "0.14.1-SNAPSHOT"
+(defproject com.github.johnwalker/bitcoin-protocol "0.14.2-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [gloss "0.2.2"]])
-
