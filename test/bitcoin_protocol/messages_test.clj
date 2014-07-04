4846b0ce70d3ddffac26de8f64ceac8dc9efbd00
John Walker <john.lou.walker@gmail.com>
2014-07-04 13:04:51 -0700

    Fix and add tests associated with netaddr change

--- test/bitcoin_protocol/messages_test.clj
+++ test/bitcoin_protocol/messages_test.clj
@@ -13,6 +13,9 @@
           (for [x (range (.remaining h))]
             (format "%02X " (.get h x))))))
 
+(defn to-unsigned [x]
+  (bit-and x 0xFF))
+
 (defn expected-bytes [length]
   (+ (dec length)
      (* 2 length)))
@@ -55,31 +58,36 @@
 (defspec isomorphic-ipaddr
   150
   (prop/for-all [ip (gen/vector gen/byte 4)]
-                (let [ubv (mapv #(bit-and % 0xFF) ip)
+                (let [ubv (mapv to-unsigned ip)
                       ip-str (apply str (interpose "." ubv))]
                   (= ip-str (decode pm/ip-addr (encode pm/ip-addr ip-str))))))
 
 
 (deftest raw-netaddrt-encoding
-  (let [[nettime
-         services
-         ip
-         port]
-        (map str-bytes (encode pm/net-addrt
-                               [1
-                                1
-                                "192.168.1.1"
-                                0xFF]))]
-    ;; Timestamp
-    ;; 4 bytes in timestamp
-    (is (= (count nettime) (expected-bytes 4)))
-    ;; Timestamp example encoded as expected (4 byte Little Endian)
-    (is (= nettime "01 00 00 00"))
-    ;; Services
-    ;; 8 bytes in services
-    (is (= (count services) (expected-bytes 8)))
-    ;; Services example encoded as expected (8 byte Little Endian)
-    (is (= services "01 00 00 00 00 00 00 00"))))
+  (= (apply str-bytes (encode pm/net-addrt
+                              {:time 1
+                               :services 1
+                               :ip "192.168.1.1"
+                               :port 0xFF}))
+     "01 00 00 00 01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 FF FF C0 A8 01 01 00 FF"))
+
+(defspec isomorphic-netaddrt
+  150
+  (prop/for-all [ip (gen/vector gen/byte 4)
+                 i gen/pos-int
+                 j gen/pos-int
+                 k gen/byte]
+                (let [ubv (mapv to-unsigned ip)
+                      ip-str (apply str (interpose "." ubv))
+                      k (to-unsigned k)]
+                  (= {:time i
+                      :services j
+                      :ip ip-str
+                      :port k}
+                     (decode pm/net-addrt (encode pm/net-addrt {:time i
+                                                                :services j
+                                                                :ip ip-str
+                                                                :port k}))))))
 
 (deftest raw-checksum
   ;; First four bytes of hello double SHA-256 is '95 95 C9 DF'
@@ -95,7 +103,7 @@
 (defspec isomorphic-checksum
   150
   (prop/for-all [i (gen/vector gen/byte 4)]
-                (let [ubv (mapv #(bit-and % 0xFF) i)]
+                (let [ubv (mapv to-unsigned i)]
                   (= ubv (decode pm/checksum (encode pm/checksum ubv))))))
 
 
@@ -126,23 +134,34 @@
   ;; 0F 2F 53 61 74 6F 73 68 69 3A 30 2E 37 2E 32 2F                               - "/Satoshi:0.7.2/" sub-version string (string is 15 bytes long)
   ;; C0 3E 03 00                                                                   - Last block sending node has is block #212672
 
-  (is (= (->> {:version 60002 :services 1 :timestamp 1355854353 :addr-recv [1 "0.0.0.0" 0] :addr-from [1 "0.0.0.0" 0] :nonce 7284544412836900411 :user-agent "/Satoshi:0.7.2/" :start-height 212672 :relay true}
+  (is (= (->> {:version 60002 :services 1 :timestamp 1355854353
+               :addr-recv {:services 1
+                           :ip "0.0.0.0"
+                           :port 0}
+               :addr-from {:services 1
+                           :ip "0.0.0.0"
+                           :port 0}
+               :nonce 7284544412836900411
+               :user-agent "/Satoshi:0.7.2/"
+               :start-height 212672
+               :relay true}
               (encode pm/version-payload)
-              (map str-bytes))
-         '("62 EA 00 00"
-           "01 00 00 00 00 00 00 00"
-           "11 B2 D0 50 00 00 00 00"
-           "01 00 00 00 00 00 00 00"
-           "00 00 00 00 00 00 00 00 00 00 FF FF 00 00 00 00"
-           "00 00"
-           "01 00 00 00 00 00 00 00"
-           "00 00 00 00 00 00 00 00 00 00 FF FF 00 00 00 00"
-           "00 00"
-           "3B 2E B3 5D 8C E6 17 65"
-           "0F"
-           "2F 53 61 74 6F 73 68 69 3A 30 2E 37 2E 32 2F"
-           "C0 3E 03 00"
-           "01"))))
+              (map str-bytes)
+              (interpose " ")
+              (apply str))
+         (apply str (interpose " " '("62 EA 00 00"
+                                     "01 00 00 00 00 00 00 00"
+                                     "11 B2 D0 50 00 00 00 00"
+                                     "01 00 00 00 00 00 00 00"
+                                     "00 00 00 00 00 00 00 00 00 00 FF FF 00 00 00 00 00 00"
+                                     "01 00 00 00 00 00 00 00"
+                                     "00 00 00 00 00 00 00 00 00 00 FF FF 00 00 00 00 00 00"
+                                     "3B 2E B3 5D 8C E6 17 65"
+                                     "0F"
+                                     "2F 53 61 74 6F 73 68 69 3A 30 2E 37 2E 32 2F"
+                                     "C0 3E 03 00"
+                                     "01"))))))
+
 
 (deftest version-message
   ;; NOTE: There just happens to be a collision between these two
@@ -164,36 +183,37 @@
   ;;  C0 3E 03 00                                                                   - Last block sending node has is block #212672
 
 
-  (= (map str-bytes (encode pm/bitcoin-network-message
-                            {:command "version"
-                             :magic :magic-value
-                             :version 60001
-                             :services 1
-                             :timestamp 0x50D0B211
-                             :addr-recv [1 "0.0.0.0" 0]
-                             :addr-from [1 "0.0.0.0" 0]
-                             :nonce 0x6517E68C5DB32E3B
-                             :user-agent "/Satoshi:0.7.2/"
-                             :start-height 212672
-                             :relay true}))
-     '("F9 BE B4 D9"
-       "76 65 72 73 69 6F 6E 00 00 00 00 00"
-       "65"
-       "85 27 39 E3"
-       "61 EA 00 00"
-       "01 00 00 00 00 00 00 00"
-       "11 B2 D0 50 00 00 00 00"
-       "01 00 00 00 00 00 00 00"
-       "00 00 00 00 00 00 00 00 00 00 FF FF 00 00 00 00"
-       "00 00"
-       "01 00 00 00 00 00 00 00"
-       "00 00 00 00 00 00 00 00 00 00 FF FF 00 00 00 00"
-       "00 00"
-       "3B 2E B3 5D 8C E6 17 65"
-       "0F"
-       "2F 53 61 74 6F 73 68 69 3A 30 2E 37 2E 32 2F"
-       "C0 3E 03 00"
-       "01")))
+  (= (apply str (interpose " " (map str-bytes (encode pm/bitcoin-network-message
+                                                      {:command "version"
+                                                       :magic :magic-value
+                                                       :version 60001
+                                                       :services 1
+                                                       :timestamp 0x50D0B211
+                                                       :addr-recv {:services 1 :ip "0.0.0.0" :port 0}
+                                                       :addr-from {:services 1 :ip "0.0.0.0" :port 0}
+                                                       :nonce 0x6517E68C5DB32E3B
+                                                       :user-agent "/Satoshi:0.7.2/"
+                                                       :start-height 212672
+                                                       :relay true}))))
+     (apply str (interpose " " '("F9 BE B4 D9"
+                                 "76 65 72 73 69 6F 6E 00 00 00 00 00"
+                                 "65"
+                                 "85 27 39 E3"
+                                 "61 EA 00 00"
+                                 "01 00 00 00 00 00 00 00"
+                                 "11 B2 D0 50 00 00 00 00"
+                                 "01 00 00 00 00 00 00 00"
+                                 "00 00 00 00 00 00 00 00 00 00 FF FF 00 00 00 00"
+                                 "00 00"
+                                 "01 00 00 00 00 00 00 00"
+                                 "00 00 00 00 00 00 00 00 00 00 FF FF 00 00 00 00"
+                                 "00 00"
+                                 "3B 2E B3 5D 8C E6 17 65"
+                                 "0F"
+                                 "2F 53 61 74 6F 73 68 69 3A 30 2E 37 2E 32 2F"
+                                 "C0 3E 03 00"
+                                 "01")))))
+
 
 (deftest verack-message
   ;; Message header:
@@ -211,15 +231,15 @@
 
 
 (deftest addr-message
-  (is (= (map str-bytes (encode pm/bitcoin-network-message {:magic :magic-value
-                                                            :command "addr"
-                                                            :payload [[0x4D1015E2 1 "10.0.0.1" 8333]]}))
-         '("F9 BE B4 D9"
-           "61 64 64 72 00 00 00 00 00 00 00 00"
-           "1F"
-           "ED 52 39 9B"
-           "01"
-           "E2 15 10 4D"
-           "01 00 00 00 00 00 00 00"
-           "00 00 00 00 00 00 00 00 00 00 FF FF 0A 00 00 01"
-           "20 8D"))))
+  (is (= (apply str (interpose " " (map str-bytes (encode pm/bitcoin-network-message {:magic :magic-value
+                                                                                      :command "addr"
+                                                                                      :payload [{:time 0x4D1015E2 :services 1 :ip "10.0.0.1" :port 8333}]}))))
+         (apply str (interpose " " '("F9 BE B4 D9"
+                                     "61 64 64 72 00 00 00 00 00 00 00 00"
+                                     "1F"
+                                     "ED 52 39 9B"
+                                     "01"
+                                     "E2 15 10 4D"
+                                     "01 00 00 00 00 00 00 00"
+                                     "00 00 00 00 00 00 00 00 00 00 FF FF 0A 00 00 01"
+                                     "20 8D"))))))
