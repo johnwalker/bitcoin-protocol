8a2229d08f52c821439eb4ac28f12c2f9d540a72
John Walker <john.lou.walker@gmail.com>
2014-07-06 07:58:27 -0700

    Add gloss extension

---
+++ src/bitcoin_protocol/gloss/extension.clj
@@ -0,0 +1,11 @@
+(ns bitcoin-protocol.gloss.extension
+  (:require [bitcoin-protocol.gloss.extension.codecs :as codecs]
+            [gloss.core :refer [compile-frame]]))
+
+(defn header
+  "I copied ztellman here. codecs/header is slightly different."
+  [frame header->body body->header]
+  (codecs/header
+   (compile-frame frame)
+   header->body
+   body->header))
---
+++ src/bitcoin_protocol/gloss/extension/codecs.clj
@@ -0,0 +1,32 @@
+(ns bitcoin-protocol.gloss.extension.codecs
+  (:use [gloss.data bytes string primitives]
+        [gloss.core protocols structure formats])
+  (:require [gloss.core :refer [ordered-map]]))
+
+(defn header
+  "It's the same as the gloss.core.codecs/header, but in read-codec
+  the header and body are merged."
+  [codec header->body body->header]
+  (let [read-codec (compose-callback
+                    codec
+                    (fn [v b]
+                      (let [body (header->body v)
+                            [success m x] (read-bytes body b)]
+                        [success (merge m v) x])))]
+    (reify
+      Reader
+      (read-bytes [_ buf-seq]
+        (read-bytes read-codec buf-seq))
+      Writer
+      (sizeof [_]
+        nil)
+      (write-bytes [_ buf val]
+        (let [header (body->header val)
+              body (header->body header)]
+          (if (and (sizeof codec) (sizeof body))
+            (with-buffer [buf (+ (sizeof codec) (sizeof body))]
+              (write-bytes codec buf header)
+              (write-bytes body buf val))
+            (concat
+             (write-bytes codec buf header)
+             (write-bytes body buf val))))))))
