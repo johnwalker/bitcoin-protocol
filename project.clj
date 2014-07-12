1ed2fbfa425922f74204f1078652b15788e64020
John Walker <john.lou.walker@gmail.com>
2014-07-11 23:58:06 -0700

    Add URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.github.johnwalker/bitcoin-protocol "0.17.0-SNAPSHOT"
   :description "An implementation of the Bitcoin networking protocol."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/johnwalker/bitcoin-protocol"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
